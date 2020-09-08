e06fa7a6cdd4749bfdd2bebbca0c22ad3ff89185
SeanSin <seansin@chungbuk.ac.kr>
2020-09-08 22:24:27 +0900

    initalize backend project

---
+++ src/main/java/kr/ac/nnet/u300/demoday/DemodayApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.nnet.u300.demoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemodayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/nnet/u300/demoday/DemodayApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.nnet.u300.demoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
